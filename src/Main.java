4b73135717af180914870788a6382d76bf7ce613
Ekaterina Shipkova <katerinashipkova@gmail.com>
2020-07-16 20:05:31 +0400

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 14500;
+        int coefficient = 20;
+        int bonus = price / coefficient;
+        System.out.println(bonus);
+    }
+}
